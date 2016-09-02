4ab483a6cb87ccd8388b8a255653b288a09e06fd
koens <koen.sauren@gmail.com>
2016-09-02 10:49:09 +0200

    newness

--- Server/functions.js
+++ Server/functions.js
@@ -1,11 +1,24 @@
-function getWorldData() {
+function getServerData() {
     var http = new XMLHttpRequest();
     http.onreadystatechange = function() {
         if (http.readyState == 4 && http.status == 200) {
             var response = http.responseText;
-
+            var responses = response.split(",");
+            var up = responses[0];
+            var playerstotal = responses[1];
+            var worlds = getWorldData(responses[2]);
         }
     }
-    http.open("GET", "");
+    http.open("GET", "get_db.php");
     http.send();
+}
+
+function getWorldData(worldstring) {
+    var worldmap = {};
+    var parts = worldstring.split("||");
+    for (var world in parts) {
+        var partss = world.split(":");
+        worldmap[partss[0]] = partss[1];
+    }
+    return worldmap;
 }
